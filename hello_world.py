d13d26974862495d54c15a8f304b57c0178cb027
Oluwadamilola <anjorinoluwadamilola@yahoo.com>
2024-05-16 12:05:35 +0100

    feat: Add hello world file

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+print("hello, world")
+print("Hello", "World")
+print("What is your name")
+myname = "Oluwadamilola"
+print("It is good to meet you, " + "Oluwadamilola")
+print("The length of your name is:")
+print(len("Oluwadamilola"))
+print("What is your age?")
+myAge = input()
+print(
+    "You will be " + str(int(25) + 1) + " in a year."
+)  # This program is asking for my name
+print("When can we meet" + myname)
+print(len("i am very fine innit!"))
+print("i am " + str(int(25)) + "years old")
+
+print("I am " + 29 + " years old.")
