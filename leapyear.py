9ca6ad2bb10e4ed17d1587a6f1480af45b120d3d
Oluwadamilola <anjorinoluwadamilola@yahoo.com>
2024-05-19 13:14:12 +0100

    feat: nested if

--- leapyear.py
+++ leapyear.py
@@ -9,3 +9,24 @@ if ((year % 4 == 0) and (year % 100 != 0)) or (
     print("It is a leap year")
 else:
     print("It is not a leap year")
+
+year = int(input("Enter the year here: "))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(True)
+        else:
+            print(False)
+    else:
+        print(True)
+else:
+    print(False)
+
+
+year = int(input("Enter the year here: "))
+if year % 4 == 0:
+    print(True)
+if year % 100 == 0:
+    print(False)
+if year % 400 == 0:
+    print(True)
